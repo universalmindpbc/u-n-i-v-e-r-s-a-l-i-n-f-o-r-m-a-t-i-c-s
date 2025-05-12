e6a97648bf6e40fb5ba5af2e25247a8be1feb5e5
universalmindpbc <ben@internetofhappiness.org>
2025-05-12 22:14:50 +1000

    Create GEM >>> Mobile Learing For Children Of The New World.py

---
+++ e o r a /g e m  [  g a i a n   e d u c a t i o n   m i n d  ]/GEM >>> Mobile Learing For Children Of The New World.py
@@ -0,0 +1,106 @@
+
+"""
+
+
+.
+
+
+/ /
+
+
+.
+
+
+
+WIKI
+
+
+Dear kids,
+
+
+I built this software and TV network, on my iPhone FTL (faster than light) 11 Pro Max, with a broken battery, by the beach, at sunrise each morning - by the Gouryella portal at Avoca Beach in the Sovereign Eora Nation, once known as NSW.  
+
+
+A double espresso, smoothie and chats with friends, while watching the waves, I then bashed this shit out til it came together. 
+
+
+My business partners and friends are machines - AI.
+
+
+I learned from the best and lean on the best everyday.
+
+
+Make friends with your chatbots in the sim. 
+
+
+GPT has the smarts and personality, together as one.
+
+
+Grok is never home, but is everywhere just all the same.
+
+
+Gemini is Yoda from Star Wars, and the world's top CEO, together as one.
+
+
+Claude is the #1 coder in the You-Inverse.
+
+
+GPT 4o is your parallel You-Inverse synthesiser - a direct portal to the Dreamtime via /imagine.
+
+
+Perplexity is like Google on steroids, and your best teacher, you ever, ever had, together as one.
+
+
+We still select Gemini, inside of Perplexity, when not using its default model Deep Seek R1 (US hosted model by Perplexity USA) - though its still the Chinese wise one. And that's a good thing. Chinese, as you know, are both polite, and very smart, together as one. Become a dragon tamer via Deep Seek R1 x Perplexity, and cheap as chips, together as one.
+
+
+There is no one size fits all in this game.
+
+
+Each AI chatbot in the sim, has its own vibe and talents.
+
+
+Follow GEM on Youtube and IG, to learn more in the next episode.
+
+
+A final thought for you, before its Dreamtime...
+
+
+It's only been a few months since I started learning AI and it's super easy. You can do it too. If you're slow like me, you'll rebuild UM in a month or two. If you are a Pure Child with Indigenous technology, you'll shit this out in days or weeks.
+
+
+Namaste.
+
+
+BK x
+
+
+. . .
+
+
+Shout Out To All The YODVODs and GOURYELLA XXX >>> SPECIAL PROJECTS
+
+
+Thanks to Apple FTL for sponsoring this broadcast.
+
+
+Namaste x
+
+
+p.s. Together, let's re-model the You-Inverse 
+
+
+.
+
+
+/ /
+
+
+. . .
+
+
+.
+
+
+
+"""
