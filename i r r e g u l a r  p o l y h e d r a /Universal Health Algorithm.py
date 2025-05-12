f794b6713b43c381e12c3ed5f4405216f94b4044
universalmindpbc <ben@internetofhappiness.org>
2025-05-12 17:53:32 +1000

    Create Universal Health Algorithm.py
    added Tensor Kernel preset folders for CUDA, CUDA-Q, Wolfram and Chemometrics for Apple iPhone via HSI.
    
    Namaste x

---
+++ i r r e g u l a r  p o l y h e d r a /Universal Health Algorithm.py
@@ -0,0 +1,125 @@
+
+"""
+
+.
+
+
+
+/ /
+
+
+.
+
+New Algorithm
+
+.
+
+@jensen @stephen @garvan @melinda
+
+https://github.com/universalmindpbc/u-n-i-v-e-r-s-a-l-i-n-f-o-r-m-a-t-i-c-s/tree/main/i%20r%20r%20e%20g%20u%20l%20a%20r%20%20p%20o%20l%20y%20h%20e%20d%20r%20a%20
+
+.
+
+step one ) Search For Diseases, Indexing via polyhedra, identified and analysed PyTorch G (Geometry), NVIDIA Modulus and NVIDIA BioNeMo, together as one.
+
+.
+
+step two ) Index all qualified diseases above, viruses, bacteria and pathogens, identified with any or all of a 4, 8, 16, 32, 64 or 128 fold geometry, together as one.
+
+
+
+step three ) Cross reference against [ I o H ] repository of irregular polyhedra within Universal Mind PBC's GitHub, here:
+
+
+
+.
+
+step four ) Create a simple table with 
+
+.
+
+. . . . 
+
+.
+
+Column 1 = Type Of Illness (mental / physical / environmental / cancer)
+
+.
+
+Column 2 = Mortality Vector % Of Illness
+
+.
+
+Column 3 = X Fold Geometry
+
+.
+
+Column 4 = Named Irregular Polyhedra
+
+.
+
+Column 5 = Is it Platonic - Yes / No
+
+.
+
+Column 6 = Is it Fibonacci - Yes / No
+
+.
+
+Column 7 = Is it Fibonacci Modulus 9 Pisano 24 - Yes / No
+
+.
+
+Column 8 = Is it Fuzzy Phi Ratio - Yes / No
+
+.
+
+Column 9 = Continent Of Illness Most Effected
+
+.
+
+Column 10 = Socio-economic demographic of Population Most Effected (include these vectors always: Sex, Age, Race)
+
+.
+
+Column 11 = Run Universal Sequence Via TeLIS - Success - Yes / No
+
+.
+
+Column 12 = Identify primary neurotransmitter based genes expressing "4X fold" irregular polyhedra, within Fuzzy Phi Ratios.
+
+.
+
+Column 13 = Present Initial Insights, supported by JADAD > 3, IF = High Peer Reviewed Literature, within Fuzzy Phi Ratios. Preference Nature, Frontiers In, .gov, .mil. Cite findings via numeral with URL direct to journal paper.
+
+.
+
+Column 14 = Push Findings In Column 13 to Universal Mind Automated Drug Discovery Pipeline, for integration with Recursion LOWE, Phenom, Ginko BioWorks, Valence Labs, InSilico, PandaOmics, Pharma AI, InClinico, Future House and Agentic Sovereign Teams, together as one.
+
+.
+
+Column 16 = Print Tensor CUDA Kernel geometric disease preset per vector, and Tensor CUDA-Q Kernel geometric disease preset per vector, together as one. Save in this folder on Universal Mind PBC GitHub.
+
+.
+
+Namaste x
+
+.
+
+. . . . 
+
+n o s  u n u m  s u m u s
+
+. . . . 
+
+.
+
+
+
+/ /
+
+
+.
+
+
+"""
