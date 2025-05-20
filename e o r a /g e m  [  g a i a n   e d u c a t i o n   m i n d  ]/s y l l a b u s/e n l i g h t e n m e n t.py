d3ecffd165c336eea1d9a7ab567e16b8b9bf30e1
Ben Korbel <ben@internetofhappiness.org>
2025-05-20 13:12:39 +1000

    oxtr snp x maslow x hawkins
    x

---
+++ e o r a /g e m  [  g a i a n   e d u c a t i o n   m i n d  ]/s y l l a b u s/e n l i g h t e n m e n t.py
@@ -0,0 +1,37 @@
+"""
+
+.
+
+See page 22 
+
+.
+
+https://internetofhappiness.org/UniversalMind(IoH)-PhaseOneMedicalStudy.pdf#page69
+
+.
+
+OXTR SNP within Fuzzy Phi Ratios vs Maslow bs Hawkin's Enlightenment Scale, quantised to scripture, culture and prophets, together as inflammation of the face.
+
+.
+
+x = classified
+
+.
+
+s e x
+
+.
+
+s e x
+
+.
+
+s e x
+
+.
+
+n a m a s t e  x
+
+.
+
+"""
